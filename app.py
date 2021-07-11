4b34b0deaef5a54dff74e6059bdcceea417c345a
Nelio Junior <nelljunior@hotmail.com>
2021-07-11 16:45:32 -0300

    regular commit

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ Tip
 
 '''
 
-import os 
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -32,8 +31,7 @@ def page_not_found(e):
     return render_template("404.html")
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
     main()
